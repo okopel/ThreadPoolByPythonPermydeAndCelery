6e2df37b42f12af763c5a389de13d087d8ee7165
okopel <okopel@gmail.com>
2019-05-25 17:04:58 -0700

    final
    Signed-off-by: okopel <okopel@gmail.com>

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     data = {34: [5, 7, 2, 5, 5, 5, 5, 5, -5.0545], 29: [5, 7, 2, 5, 5, 5, 5, 5, -5]}
     r = requests.post(url, auth=('username', 'password'), verify=False, json=data)
     # print(r.status_code)
-    url = 'http://localhost:6543/results/320'
+    url = 'http://localhost:6543/results/34'
     p = requests.get(url)
     url = 'http://localhost:6543/results/8888880'
     p = requests.get(url)
